459530706075b0a9dbccd52936d63fda0102f98d
Toleckk <anatolich1995@gmail.com>
2020-08-25 16:19:20 +0300

    Add themes

---
+++ src/themes/dark.js
@@ -0,0 +1,13 @@
+export const dark = {
+  name: 'dark',
+
+  primary: '2, 132, 132',
+  secondary: '163, 67, 81',
+  primaryText: '186, 186, 199',
+  secondaryText: '202, 202, 220',
+  error: '193, 27, 27',
+  errorText: '255, 96, 96',
+  primaryDark: '10, 21, 27',
+  landingBackground:
+    'https://res.cloudinary.com/jumper/image/upload/v1590683915/landing_ahhvvr.jpg',
+}
