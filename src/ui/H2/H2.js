1308ae749b6d17520ea9ffe5b6523f734e740bc1
Toleckk <anatolich1995@gmail.com>
2020-08-26 03:41:32 +0300

    Add H2

---
+++ src/ui/H2/H2.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const H2 = styled.h2`
+  color: rgb(${props => props.theme.primaryText});
+  text-align: center;
+  font-size: large;
+  font-weight: normal;
+`
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,6 +1,7 @@
 export * from './Button'
 export * from './Divider'
 export * from './H1'
+export * from './H2'
 export * from './Icon'
 export * from './Input'
 export * from './Link'
