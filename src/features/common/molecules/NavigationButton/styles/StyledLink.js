fddea1c264dfc06f8df5f632719e6f047a0d95df
Toleckk <anatolich1995@gmail.com>
2020-08-26 22:34:55 +0300

    Fix NavigationButton isActive prop

--- src/features/common/molecules/NavigationButton/styles/StyledLink.js
+++ src/features/common/molecules/NavigationButton/styles/StyledLink.js
@@ -1,5 +1,6 @@
 import styled, {css} from 'styled-components'
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const activeStyle = css`
   filter: drop-shadow(0px 0px 6px rgb(${props => props.theme.secondary}));
@@ -13,5 +14,13 @@ export const StyledLink = styled(Link)`
     ${activeStyle}
   }
 
-  ${props => props.active && activeStyle}
+  ${props => props.isActive && activeStyle}
 `
+
+StyledLink.defaultProps = {
+  isActive: false,
+}
+
+StyledLink.propTypes = {
+  isActive: PropTypes.bool,
+}
