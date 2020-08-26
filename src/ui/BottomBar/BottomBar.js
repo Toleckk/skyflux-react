6d1ec99e297caa0d7bce911a5ea55c16cab62aee
Toleckk <anatolich1995@gmail.com>
2020-08-26 23:37:40 +0300

    Fix BottomBar styles

--- src/ui/BottomBar/BottomBar.js
+++ src/ui/BottomBar/BottomBar.js
@@ -3,6 +3,11 @@ import styled from 'styled-components'
 export const BottomBar = styled.div`
   position: fixed;
   bottom: 0;
+  left: 0;
+
+  width: 100%;
+
+  padding: 5px 0;
 
   box-shadow: 0 0 10px rgb(${props => props.theme.primary});
 `
