df23064eb845cc67f4f0fd5e8118106e9d64a618
Toleckk <anatolich1995@gmail.com>
2020-08-27 22:26:36 +0300

    Add recommendations when feed is empty

--- src/features/feed/pages/All/All.js
+++ src/features/feed/pages/All/All.js
@@ -1,22 +1,50 @@
 import React from 'react'
+import {Flex} from 'reflexbox/styled-components'
 import {PostForm} from '../../../common/organisms'
-import {PostCard} from '../../../common/molecules'
-import {Divider} from '../../../../ui'
+import {PostCard, UserCard} from '../../../common/molecules'
+import {Divider, H1, Link, Text} from '../../../../ui'
 import {StyledContainer} from './styles'
 
 const postsMock = [
+  // {
+  //   _id: '1',
+  //   text: '123456',
+  //   date: '123456',
+  //   commentsCount: 1,
+  //   likesCount: 2,
+  //   hasMyLike: true,
+  //   user: {
+  //     nickname: 'toleckk',
+  //     avatar:
+  //       'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+  //   },
+  // },
+]
+
+const users = [
   {
-    _id: '1',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
+    _id: 1,
+    nickname: 'toleckk',
+    avatar:
+      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+  },
+  {
+    _id: 2,
+    nickname: 'toleckk',
+    avatar:
+      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+  },
+  {
+    _id: 3,
+    nickname: 'toleckk',
+    avatar:
+      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+  },
+  {
+    _id: 4,
+    nickname: 'toleckk',
+    avatar:
+      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
   },
 ]
 
@@ -24,8 +52,26 @@ export const All = () => (
   <StyledContainer>
     <PostForm />
     <Divider />
-    {postsMock.map(post => (
-      <PostCard post={post} key={post._id} />
-    ))}
+    {postsMock.length ? (
+      postsMock.map(post => <PostCard post={post} key={post._id} />)
+    ) : (
+      <div>
+        <H1>
+          Ваша лента пуста! Осмотритесь, чтобы найти интересных пользователей
+        </H1>
+        <Flex as="ul" justifyContent="space-between" margin="1rem 0">
+          {users.map(user => (
+            <li key={user._id}>
+              <UserCard user={user} key={user} />
+            </li>
+          ))}
+        </Flex>
+        <Text>
+          Если ищете что-то, Вы можете воспользоваться нашим{' '}
+          <Link to="/search">поиском</Link>
+        </Text>
+        <Divider />
+      </div>
+    )}
   </StyledContainer>
 )
