e20573f31f181fca621e5be9ce1cf31a807be226
Toleckk <anatolich1995@gmail.com>
2020-08-27 22:25:30 +0300

    Add hygen component generator

---
+++ _templates/component/with-prompt/prompt.js
@@ -0,0 +1,15 @@
+// see types of prompts:
+// https://github.com/enquirer/enquirer/tree/master/examples
+//
+module.exports = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your name?",
+  },
+  {
+    type: 'input',
+    name: 'path',
+    message: 'What is path to folder?',
+  },
+]
