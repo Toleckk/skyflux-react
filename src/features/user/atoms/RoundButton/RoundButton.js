dd442c6f5b665ec748439a6c32d44d9a34f04c97
Toleckk <anatolich1995@gmail.com>
2020-08-29 00:48:05 +0300

    Add RoundButton

---
+++ src/features/user/atoms/RoundButton/RoundButton.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import {Button} from '../../../../ui'
+
+export const RoundButton = styled(Button)`
+  padding: 0.5em 1.5em;
+  border-radius: 1.5em;
+`
--- src/features/user/atoms/index.js
+++ src/features/user/atoms/index.js
@@ -1,2 +1,3 @@
 export * from './About'
 export * from './BigNickname'
+export * from './RoundButton'
