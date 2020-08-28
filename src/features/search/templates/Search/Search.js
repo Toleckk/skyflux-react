ec66408b77ea4d065cd831359bd6294a933ed490
Toleckk <anatolich1995@gmail.com>
2020-08-28 03:32:28 +0300

    Add Search pages template

---
+++ src/features/search/templates/Search/Search.js
@@ -0,0 +1,39 @@
+import React, {useCallback, useState} from 'react'
+import PropTypes from 'prop-types'
+import {Flex, Box} from 'reflexbox/styled-components'
+import {SearchInput} from '../../molecules'
+import {H1} from '../../../../ui'
+import {SearchLoader} from '../../atoms'
+
+export const Search = ({onInputChange, isLoading, children}) => {
+  const [text, setText] = useState('')
+
+  const onChange = useCallback(
+    e => {
+      setText(e.target.value)
+      return onInputChange?.(e)
+    },
+    [setText, onInputChange],
+  )
+
+  return (
+    <Flex flexDirection="column" minHeight="100vh" paddingTop="2rem">
+      <Flex flex={text ? 0 : 1} alignItems="center">
+        <SearchInput value={text} onChange={onChange} />
+      </Flex>
+      {isLoading && !!text && <SearchLoader />}
+      {!isLoading && !children && !!text && (
+        <Box margin="auto">
+          <H1>Ничего не найдено!</H1>
+        </Box>
+      )}
+      {!isLoading && !!text && children}
+    </Flex>
+  )
+}
+
+Search.propTypes = {
+  onInputChange: PropTypes.func,
+  isLoading: PropTypes.bool,
+  children: PropTypes.node,
+}
