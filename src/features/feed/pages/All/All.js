35e313b7364f12fad5751b1fae2c77c7ecfd031f
Toleckk <anatolich1995@gmail.com>
2020-08-28 03:28:54 +0300

    Inject lists into feed main page

--- src/features/feed/pages/All/All.js
+++ src/features/feed/pages/All/All.js
@@ -1,24 +1,63 @@
 import React from 'react'
-import {Flex} from 'reflexbox/styled-components'
+import {Box} from 'reflexbox/styled-components'
 import {PostForm} from '../../../common/organisms'
-import {PostCard, UserCard} from '../../../common/molecules'
+import {PostList, UserList} from '../../../common/molecules'
 import {Divider, H1, Link, Text} from '../../../../ui'
 import {StyledContainer} from './styles'
 
 const postsMock = [
-  // {
-  //   _id: '1',
-  //   text: '123456',
-  //   date: '123456',
-  //   commentsCount: 1,
-  //   likesCount: 2,
-  //   hasMyLike: true,
-  //   user: {
-  //     nickname: 'toleckk',
-  //     avatar:
-  //       'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-  //   },
-  // },
+  {
+    _id: '1',
+    text: '123456',
+    date: '123456',
+    commentsCount: 1,
+    likesCount: 2,
+    hasMyLike: true,
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    _id: '2',
+    text: '123456',
+    date: '123456',
+    commentsCount: 1,
+    likesCount: 2,
+    hasMyLike: true,
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    _id: '3',
+    text: '123456',
+    date: '123456',
+    commentsCount: 1,
+    likesCount: 2,
+    hasMyLike: true,
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    _id: '4',
+    text: '123456',
+    date: '123456',
+    commentsCount: 1,
+    likesCount: 2,
+    hasMyLike: true,
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
 ]
 
 const users = [
@@ -53,19 +92,15 @@ export const All = () => (
     <PostForm />
     <Divider />
     {postsMock.length ? (
-      postsMock.map(post => <PostCard post={post} key={post._id} />)
+      <PostList posts={postsMock} />
     ) : (
       <div>
         <H1>
           Ваша лента пуста! Осмотритесь, чтобы найти интересных пользователей
         </H1>
-        <Flex as="ul" justifyContent="space-between" margin="1rem 0">
-          {users.map(user => (
-            <li key={user._id}>
-              <UserCard user={user} key={user} />
-            </li>
-          ))}
-        </Flex>
+        <Box margin="1rem 0">
+          <UserList users={users} />
+        </Box>
         <Text>
           Если ищете что-то, Вы можете воспользоваться нашим{' '}
           <Link to="/search">поиском</Link>
