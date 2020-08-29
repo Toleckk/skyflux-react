70da48c43fa42268ae762e902fb206cb919bfea7
Toleckk <anatolich1995@gmail.com>
2020-08-29 23:23:37 +0300

    Add Notifications route

--- src/features/common/organisms/DesktopNav/DesktopNav.js
+++ src/features/common/organisms/DesktopNav/DesktopNav.js
@@ -1,32 +1,195 @@
 import React from 'react'
-import {Flex} from 'reflexbox/styled-components'
-import {Avatar} from '../../../../ui'
-import {NavigationButton} from '../../molecules'
+import {Route} from 'react-router-dom'
+import {useLocation} from 'react-use'
+import {Box, Flex} from 'reflexbox/styled-components'
+import {Avatar, H2} from '../../../../ui'
+import {EventList, NavigationButton} from '../../molecules'
 import {StyledItem} from './styles'
 
-export const DesktopNav = () => (
-  <Flex as="nav">
-    <ul>
-      <StyledItem>
-        <NavigationButton>
-          <Avatar src="https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg" />
-        </NavigationButton>
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton to="/feed" icon="feed" />
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton icon="notifications" />
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton icon="settings" to="/settings" />
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton icon="search" to="/search" />
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton icon="logout" />
-      </StyledItem>
-    </ul>
-  </Flex>
-)
+const events = [
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+].map((event, _id) => ({...event, _id}))
+
+export const DesktopNav = () => {
+  const {pathname} = useLocation()
+  return (
+    <Flex as="nav">
+      <ul>
+        <StyledItem>
+          <NavigationButton>
+            <Avatar src="https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg" />
+          </NavigationButton>
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton to="/feed" icon="feed" />
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton
+            icon="notifications"
+            to={
+              pathname.match('notifications')
+                ? pathname.replace('/notifications', '')
+                : pathname + '/notifications'
+            }
+          />
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton icon="settings" to="/settings" />
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton icon="search" to="/search" />
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton icon="logout" />
+        </StyledItem>
+      </ul>
+      <Route path="**/notifications**">
+        <Flex flexDirection="column" paddingRight="1rem">
+          <Flex
+            flexBasis="0px"
+            flexGrow="1"
+            overflow="hidden"
+            flexDirection="column"
+          >
+            <Box margin="0 2rem">
+              <H2>Запросы на подписку</H2>
+            </Box>
+            <EventList events={events} />
+          </Flex>
+        </Flex>
+      </Route>
+    </Flex>
+  )
+}
--- src/features/common/templates/Navigable/Navigable.js
+++ src/features/common/templates/Navigable/Navigable.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Flex} from 'reflexbox/styled-components'
-import {useMedia} from 'react-use'
+import {useLocation, useMedia} from 'react-use'
 import {DesktopNav, MobileNav} from '../../organisms'
 import {BottomBar, SideBar} from '../../../../ui'
 
 export const Navigable = ({children}) => {
   const isDesktop = useMedia('(min-width: 768px)')
+  const {pathname} = useLocation()
+  const isNotificationsOpened = !!pathname.match('notifications')
 
   return (
     <Flex justifyContent="center">
@@ -14,7 +16,10 @@ export const Navigable = ({children}) => {
         {children}
       </Flex>
       {isDesktop ? (
-        <SideBar minHeight="6rem">
+        <SideBar
+          minHeight={isNotificationsOpened || '6rem'}
+          border={isNotificationsOpened || 'hover'}
+        >
           <DesktopNav />
         </SideBar>
       ) : (
