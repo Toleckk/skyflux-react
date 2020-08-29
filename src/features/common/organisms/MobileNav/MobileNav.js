ea3e87ce04b76d7709a9d3f2cf38d49bb2f993c0
Toleckk <anatolich1995@gmail.com>
2020-08-30 00:14:57 +0300

    Add notifications drawer to MobileNav

--- src/features/common/organisms/MobileNav/MobileNav.js
+++ src/features/common/organisms/MobileNav/MobileNav.js
@@ -1,26 +1,176 @@
 import React from 'react'
-import {Flex} from 'reflexbox/styled-components'
-import {Avatar} from '../../../../ui'
-import {NavigationButton} from '../../molecules'
+import {Box, Flex} from 'reflexbox/styled-components'
+import {Drawer} from 'react-pretty-drawer'
+import {Avatar, H2} from '../../../../ui'
+import {EventList, NavigationButton} from '../../molecules'
+import {useNotificationsDisplay} from '../../hooks'
 import {StyledItem} from './styles'
 
-export const MobileNav = () => (
-  <nav>
-    <Flex as="ul" justifyContent="space-evenly">
-      <StyledItem>
-        <NavigationButton icon="search" to="/search" />
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton icon="notifications" />
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton icon="feed" to="/feed" />
-      </StyledItem>
-      <StyledItem>
-        <NavigationButton>
-          <Avatar src="https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg" />
-        </NavigationButton>
-      </StyledItem>
-    </Flex>
-  </nav>
-)
+const events = [
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+  {
+    user: {
+      nickname: 'toleckk',
+      avatar:
+        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
+    },
+  },
+].map((event, _id) => ({...event, _id}))
+
+export const MobileNav = () => {
+  const {close, toggle, isOpened} = useNotificationsDisplay()
+
+  return (
+    <nav>
+      <Flex as="ul" justifyContent="space-evenly">
+        <StyledItem>
+          <NavigationButton icon="search" to="/search" />
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton icon="notifications" onClick={toggle} />
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton icon="feed" to="/feed" />
+        </StyledItem>
+        <StyledItem>
+          <NavigationButton>
+            <Avatar src="https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg" />
+          </NavigationButton>
+        </StyledItem>
+      </Flex>
+      <Drawer visible={isOpened} onClose={close} placement="right">
+        <Flex flexDirection="column" overflow="hidden" height="100%">
+          <H2>Запросы на подписку</H2>
+          <Box flexGrow={1} flexBasis={0} overflow="auto">
+            <EventList events={events} />
+          </Box>
+        </Flex>
+      </Drawer>
+    </nav>
+  )
+}
