aa141e75d4109efc2bec2b78871a9ad83a58353b
Toleckk <anatolich1995@gmail.com>
2020-08-29 17:29:42 +0300

    Add CommentCard

---
+++ src/features/common/molecules/CommentCard/CommentCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {PublicationCard} from '../index'
+
+export const CommentCard = ({publication}) => (
+  <PublicationCard publication={publication} />
+)
+
+CommentCard.propTypes = {
+  publication: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    user: PropTypes.shape({
+      avatar: PropTypes.string,
+      nickname: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+}
--- src/features/common/molecules/index.js
+++ src/features/common/molecules/index.js
@@ -1,3 +1,4 @@
+export * from './CommentCard'
 export * from './NavigationButton'
 export * from './PostCard'
 export * from './PostInput'
