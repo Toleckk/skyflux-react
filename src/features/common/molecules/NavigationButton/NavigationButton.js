21743d3eb91c6637320e5678bbfe30da5588950d
Toleckk <anatolich1995@gmail.com>
2020-08-29 23:56:40 +0300

    Add isActive drilling to NavigationButton

--- src/features/common/molecules/NavigationButton/NavigationButton.js
+++ src/features/common/molecules/NavigationButton/NavigationButton.js
@@ -1,16 +1,19 @@
 import React, {useMemo} from 'react'
 import {useLocation} from 'react-router'
 import PropTypes from 'prop-types'
-import {Icon} from '../../../../ui'
 import {StyledIcon, StyledLink} from './styles'
 
-export const NavigationButton = ({to, children, icon, ...props}) => {
+export const NavigationButton = ({to, children, isActive, icon, ...props}) => {
   const {pathname} = useLocation()
 
   const linkProps = useMemo(() => ({...(to ? {to} : {as: 'button'})}), [to])
 
   return (
-    <StyledLink isActive={pathname.startsWith(to)} {...linkProps} {...props}>
+    <StyledLink
+      isActive={isActive || (to && !!pathname.match(to))}
+      {...linkProps}
+      {...props}
+    >
       {icon ? <StyledIcon icon={icon} /> : children}
     </StyledLink>
   )
@@ -19,5 +22,6 @@ export const NavigationButton = ({to, children, icon, ...props}) => {
 NavigationButton.propTypes = {
   to: PropTypes.string,
   children: PropTypes.node,
-  icon: Icon.propTypes.icon,
+  icon: PropTypes.string,
+  isActive: PropTypes.bool,
 }
