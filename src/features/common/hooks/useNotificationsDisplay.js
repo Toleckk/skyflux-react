ec2d5cdd94efa506f26a693af5e907cf4c46c73c
Toleckk <anatolich1995@gmail.com>
2020-08-29 23:55:07 +0300

    Add useNotificationsDisplay

---
+++ src/features/common/hooks/useNotificationsDisplay.js
@@ -0,0 +1,28 @@
+import {useHistory, useLocation} from 'react-router'
+import {useCallback} from 'react'
+
+export const useNotificationsDisplay = () => {
+  const {pathname} = useLocation()
+  const history = useHistory()
+
+  const open = useCallback(() => history.push(pathname + '/notifications'), [
+    pathname,
+    history,
+  ])
+
+  const close = useCallback(
+    () => history.push(pathname.replace('/notifications', '')),
+    [pathname, history],
+  )
+
+  const isOpened = !!pathname.match('/notifications')
+
+  const toggle = useCallback(isOpened ? close : open, [isOpened, close, open])
+
+  return {
+    open,
+    toggle,
+    close,
+    isOpened,
+  }
+}
