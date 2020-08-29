685d914848c2eac3d73a3721875d307fddecb28a
Toleckk <anatolich1995@gmail.com>
2020-08-29 23:22:23 +0300

    Fix props drilling in NavigationButton

--- src/features/common/molecules/NavigationButton/NavigationButton.js
+++ src/features/common/molecules/NavigationButton/NavigationButton.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 import {Icon} from '../../../../ui'
 import {StyledIcon, StyledLink} from './styles'
 
-export const NavigationButton = ({to, children, icon}) => {
+export const NavigationButton = ({to, children, icon, ...props}) => {
   const {pathname} = useLocation()
 
-  const props = useMemo(() => ({...(to ? {to} : {as: 'button'})}), [to])
+  const linkProps = useMemo(() => ({...(to ? {to} : {as: 'button'})}), [to])
 
   return (
-    <StyledLink {...props} isActive={pathname.startsWith(to)}>
+    <StyledLink isActive={pathname.startsWith(to)} {...linkProps} {...props}>
       {icon ? <StyledIcon icon={icon} /> : children}
     </StyledLink>
   )
