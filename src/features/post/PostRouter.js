68e1c575e4f727bd51f8669bec7343a5f9eca8ed
Toleckk <anatolich1995@gmail.com>
2020-08-30 21:14:53 +0300

    Add PostRouter

---
+++ src/features/post/PostRouter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Route} from 'react-router'
+import {useModal} from '../common/hooks'
+import {Display} from './pages'
+
+export const PostRouter = () => {
+  const {isOpened} = useModal('post')
+
+  return (
+    <>
+      <Route path="/post/:id" component={Display} />
+      {isOpened && <Display />}
+    </>
+  )
+}
