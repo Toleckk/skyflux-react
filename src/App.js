36d5f3b490ff7e8a62687006039a6157498bc79d
Toleckk <anatolich1995@gmail.com>
2020-08-30 21:33:06 +0300

    Add application router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
 import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import {ThemeProvider} from 'styled-components'
+import {Route, Switch} from 'react-router'
+import {dark} from './themes'
+import {Navigable} from './features/common/templates'
+import {IdRouter} from './features/id'
+import {UserRouter} from './features/user'
+import {FeedRouter} from './features/feed'
+import {PostRouter} from './features/post'
+import {SearchRouter} from './features/search'
+import {SettingsRouter} from './features/settings'
 
-export const App = () => <></>
+export const App = () => (
+  <ThemeProvider theme={dark}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/id">
+          <IdRouter />
+        </Route>
+        <Navigable>
+          <FeedRouter />
+          <PostRouter />
+          <SearchRouter />
+          <SettingsRouter />
+          <UserRouter />
+        </Navigable>
+      </Switch>
+    </BrowserRouter>
+  </ThemeProvider>
+)
