ca201605d82a688e7247bfb1117cdb85e2fe20e8
Toleckk <anatolich1995@gmail.com>
2020-09-02 11:32:04 +0300

    Implement PostForm validation

--- src/features/common/organisms/PostForm/PostForm.js
+++ src/features/common/organisms/PostForm/PostForm.js
@@ -1,22 +1,37 @@
 import React, {useCallback, useState} from 'react'
 import PropTypes from 'prop-types'
+import {useForm} from 'react-hook-form'
+import {yupResolver} from '@hookform/resolvers'
+import * as yup from 'yup'
 import {Icon} from '../../../../ui'
 import {PostInput} from '../../molecules'
 import {StyledButton, StyledContainer} from './styles'
 
+const schema = yup.object().shape({
+  text: yup.string().max(120).required(),
+})
+
 export const PostForm = ({placeholder}) => {
+  const {register, handleSubmit} = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = useCallback(handleSubmit(console.log), [handleSubmit])
+
   const [isFocused, setFocused] = useState(false)
 
   const onFocus = useCallback(() => setFocused(true), [setFocused])
   const onBlur = useCallback(() => setFocused(false), [setFocused])
 
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <StyledContainer>
         <PostInput
+          name="text"
           onBlur={onBlur}
           onFocus={onFocus}
           placeholder={placeholder}
+          ref={register}
         />
         {isFocused && (
           <StyledButton>
@@ -24,7 +39,7 @@ export const PostForm = ({placeholder}) => {
           </StyledButton>
         )}
       </StyledContainer>
-    </div>
+    </form>
   )
 }
 
