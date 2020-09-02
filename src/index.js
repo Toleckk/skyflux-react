418ac984b7497389d1748a1fe9b0acb8e689268c
Toleckk <anatolich1995@gmail.com>
2020-09-02 23:51:04 +0300

    Shorten imports in src

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'normalize.css'
 import 'focus-within-polyfill'
 import './index.css'
 import './configs'
-import {App} from './App'
+import {App} from 'App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
