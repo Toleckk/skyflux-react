c8b078fd5795301caffd049c8c1e8d91f5e1a326
Toleckk <anatolich1995@gmail.com>
2020-09-02 12:28:54 +0300

    Fix translation '...' bug

--- src/features/id/pages/Reset/Reset.js
+++ src/features/id/pages/Reset/Reset.js
@@ -10,7 +10,7 @@ export const Reset = withTranslation('id')(({t}) => (
     <H1>{t('Create new password')}</H1>
     <PageDescription>{t('Active sessions will be canceled')}</PageDescription>
     <Divider />
-    <FieldDescription>{t('Password should be...')}</FieldDescription>
+    <FieldDescription>{t('Password should be')}</FieldDescription>
     <Box width="35%">
       <ResetForm />
     </Box>
