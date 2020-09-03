457e8037bd1a5cad6f47b4e95ca50463459ac12a
Toleckk <anatolich1995@gmail.com>
2020-09-03 23:36:20 +0300

    Implement search pages data querying

--- src/features/search/pages/Posts/Posts.js
+++ src/features/search/pages/Posts/Posts.js
@@ -1,67 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Box} from 'reflexbox/styled-components'
+import {useMyQuery} from 'features/common/hooks'
+import {getFoundPosts} from 'models/post'
 import {Search} from '../../templates'
 import {PostsDisplay} from '../../organisms'
 
-const posts = [
-  {
-    _id: '1',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-  {
-    _id: '2',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-  {
-    _id: '3',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-  {
-    _id: '4',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-]
-
 export const Posts = () => {
+  const [text, setText] = useState('')
+  const {data, loading} = useMyQuery(getFoundPosts(text))
+
+  const posts = data?.getFoundPosts?.edges
+
   return (
-    <Search>
-      {posts && !!posts.length && (
+    <Search onInputChange={setText} isLoading={loading}>
+      {!!posts?.length && (
         <Box marginTop="2rem">
           <PostsDisplay posts={posts} />
         </Box>
