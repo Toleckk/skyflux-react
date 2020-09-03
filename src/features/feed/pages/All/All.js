10c57e07072338e42d4d523845913b97431b5932
Toleckk <anatolich1995@gmail.com>
2020-09-03 23:36:50 +0300

    Implement feed data querying

--- src/features/feed/pages/All/All.js
+++ src/features/feed/pages/All/All.js
@@ -3,110 +3,46 @@ import {Box} from 'reflexbox/styled-components'
 import {Trans, withTranslation} from 'react-i18next'
 import {PostForm} from 'features/common/organisms'
 import {PostList, UserList} from 'features/common/molecules'
-import {Divider, H1, Link, Text} from 'ui'
+import {useMyQuery} from 'features/common/hooks'
+import {getSuggestions} from 'models/user'
+import {getFeed} from 'models/post'
+import {Divider, H1, Link, Loader, Text} from 'ui'
 import {StyledContainer} from './styles'
 
-const postsMock = [
-  {
-    _id: '1',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-  {
-    _id: '2',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-  {
-    _id: '3',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-  {
-    _id: '4',
-    text: '123456',
-    date: '123456',
-    commentsCount: 1,
-    likesCount: 2,
-    hasMyLike: true,
-    user: {
-      nickname: 'toleckk',
-      avatar:
-        'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-    },
-  },
-]
+export const All = withTranslation('feed')(({t}) => {
+  const {data, loading} = useMyQuery(getFeed())
+  const {data: suggestionsData, loading: suggestionsLoading} = useMyQuery(
+    getSuggestions(),
+  )
+  const posts = data?.getFeed?.edges
+  const suggestions = suggestionsData?.getSuggestions?.edges
 
-const users = [
-  {
-    _id: 1,
-    nickname: 'toleckk',
-    avatar:
-      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-  },
-  {
-    _id: 2,
-    nickname: 'toleckk',
-    avatar:
-      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-  },
-  {
-    _id: 3,
-    nickname: 'toleckk',
-    avatar:
-      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-  },
-  {
-    _id: 4,
-    nickname: 'toleckk',
-    avatar:
-      'https://res.cloudinary.com/jumper/image/upload/v1591605952/images/cuv6hqfjc8dhh9igsclt.jpg',
-  },
-]
-
-export const All = withTranslation('feed')(({t}) => (
-  <StyledContainer>
-    <PostForm placeholder={t('Write a news')} />
-    <Divider />
-    {postsMock.length ? (
-      <PostList posts={postsMock} />
-    ) : (
-      <div>
-        <H1>{t('Your feed is empty')}</H1>
-        <Box margin="1rem 0">
-          <UserList users={users} />
-        </Box>
-        <Text>
-          <Trans t={t}>
-            If look smth, you can use our <Link to={'/search'}>search</Link>
-          </Trans>
-        </Text>
-        <Divider />
-      </div>
-    )}
-  </StyledContainer>
-))
+  return (
+    <StyledContainer>
+      <PostForm placeholder={t('Write a news')} />
+      <Divider />
+      {loading ? (
+        <Loader />
+      ) : posts?.length ? (
+        <PostList posts={posts} />
+      ) : (
+        <div>
+          <H1>{t('Your feed is empty')}</H1>
+          <Box margin="1rem 0">
+            {suggestionsLoading || !suggestions ? (
+              <Loader />
+            ) : (
+              <UserList users={suggestions} />
+            )}
+          </Box>
+          <Text>
+            <Trans t={t}>
+              If look smth, you can use our <Link to={'/search'}>search</Link>
+            </Trans>
+          </Text>
+          <Divider />
+        </div>
+      )}
+    </StyledContainer>
+  )
+})
