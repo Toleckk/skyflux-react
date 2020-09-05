cb9238c6b3056c277e40d8edbb176a943c2e023b
Toleckk <anatolich1995@gmail.com>
2020-09-05 23:30:12 +0300

    Extend useMyMutation for passing mutation variables to refetchQueries callback

--- src/features/common/hooks/useMyMutation.js
+++ src/features/common/hooks/useMyMutation.js
@@ -1,10 +1,34 @@
 import {useMutation} from '@apollo/client'
 import {useCallback} from 'react'
 
-export const useMyMutation = ({mutation, ...options}) => {
+export const useMyMutation = ({
+  mutation,
+  refetchQueries,
+  variables,
+  ...options
+}) => {
   const [mutate, ...rest] = useMutation(mutation, options)
 
-  const mutateVars = useCallback(variables => mutate({variables}), [mutate])
+  const refetch = useCallback(
+    (mutateVariables = {}) => data => {
+      if (Array.isArray(refetchQueries)) return refetchQueries
+
+      if (typeof refetchQueries === 'function')
+        return refetchQueries(data, {...variables, ...mutateVariables})
+
+      return []
+    },
+    [refetchQueries, variables],
+  )
+
+  const mutateVars = useCallback(
+    (mutateVariables = {}) =>
+      mutate({
+        variables: {...variables, ...mutateVariables},
+        refetchQueries: refetch(mutateVariables),
+      }),
+    [mutate, refetch, variables],
+  )
 
   return [mutateVars, ...rest]
 }
