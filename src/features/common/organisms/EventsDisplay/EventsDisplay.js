efa2378a432a9b15709db5abe54de1621a81541b
Toleckk <anatolich1995@gmail.com>
2020-09-07 12:45:56 +0300

    Implement events pagination

--- src/features/common/organisms/EventsDisplay/EventsDisplay.js
+++ src/features/common/organisms/EventsDisplay/EventsDisplay.js
@@ -2,14 +2,25 @@ import React from 'react'
 import {useMyQuery} from 'features/common/hooks'
 import {getEvents} from 'models/event'
 import {Loader} from 'ui'
+import {useInfiniteScroll} from 'useInfiniteScroll'
 import {EventList} from '../../molecules'
 
 export const EventsDisplay = () => {
-  const {data, loading} = useMyQuery(getEvents())
+  const {data, loading, fetchMore} = useMyQuery({
+    ...getEvents(),
+    variables: {first: 25},
+  })
 
   const events = data?.getEvents?.edges
 
+  const ref = useInfiniteScroll({
+    fetchMore,
+    loading,
+    hasMore: data?.getEvents?.pageInfo?.hasNextPage,
+    threshold: '100px',
+  })
+
   if (loading) return <Loader />
 
-  return <EventList events={events} />
+  return <EventList events={events} ref={ref} />
 }
