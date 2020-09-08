c261d55ee4337e3e2d7cc516412a2b95ae25807c
Toleckk <anatolich1995@gmail.com>
2020-09-08 12:05:31 +0300

    Implement passing authorization token via headers

--- src/configs/apollo.js
+++ src/configs/apollo.js
@@ -1,6 +1,21 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client'
+import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client'
+import {setContext} from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_API_URL,
+})
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 
 export const client = new ApolloClient({
-  uri: process.env.REACT_APP_API_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
