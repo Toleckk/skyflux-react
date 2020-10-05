9937bdf9a7954cf325d41ac9db43a0505dd0063d
Toleckk <anatolich1995@gmail.com>
2020-10-05 19:26:46 +0300

    Rework event model schema

--- src/models/event/schemas.js
+++ src/models/event/schemas.js
@@ -1,12 +1,32 @@
 import {gql} from '@apollo/client'
 
-export const EventFragment = gql`
-  fragment EventFragment on Event {
-    _id
-    user {
-      _id
-      nickname
-      avatar
+export const MiniSubEventFragment = gql`
+  fragment MiniSubEventFragment on SubEvent {
+    kind
+    subj {
+      sub {
+        accepted
+        from {
+          _id
+          avatar
+          nickname
+        }
+      }
+    }
+  }
+`
+
+export const MiniCommentEventFragment = gql`
+  fragment MiniCommentEventFragment on CommentEvent {
+    kind
+    subj {
+      comment {
+        text
+        user {
+          avatar
+          nickname
+        }
+      }
     }
   }
 `
@@ -23,10 +43,12 @@ export const GET_EVENTS = gql`
       edges {
         cursor
         node {
-          ...EventFragment
+          ...MiniCommentEventFragment
+          ...MiniSubEventFragment
         }
       }
     }
   }
-  ${EventFragment}
+  ${MiniSubEventFragment}
+  ${MiniCommentEventFragment}
 `
