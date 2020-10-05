d655a18770aa892704f43a1a7a4d8df285b11ecd
Toleckk <anatolich1995@gmail.com>
2020-10-05 19:21:51 +0300

    Delete old events displaying

--- src/features/common/molecules/EventList/EventList.js
+++ src/features/common/molecules/EventList/EventList.js
@@ -1,20 +1,3 @@
-import React, {forwardRef} from 'react'
-import {EventConnectionList} from 'models/event'
-import {EventCard} from '../EventCard'
-import {StyledItem, StyledList} from './styles'
+import React from 'react'
 
-export const EventList = forwardRef(({events}, ref) => (
-  <StyledList ref={ref}>
-    {events.map(event => (
-      <StyledItem key={event.cursor}>
-        <EventCard event={event.node} key={event.cursor} />
-      </StyledItem>
-    ))}
-  </StyledList>
-))
-
-EventList.displayName = 'EventList'
-
-EventList.propTypes = {
-  events: EventConnectionList.isRequired,
-}
+export const EventList = () => <></>
