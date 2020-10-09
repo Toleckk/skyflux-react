3215d328f939ec450109a3943dda34b0d2dc55a3
Toleckk <anatolich1995@gmail.com>
2020-10-10 00:15:20 +0300

    Add EventList

--- src/features/common/molecules/EventList/EventList.js
+++ src/features/common/molecules/EventList/EventList.js
@@ -1,3 +1,14 @@
-import React from 'react'
+import React, {forwardRef} from 'react'
+import {EventConnectionList} from 'models/event'
+import {PublicationList} from '../PublicationList'
+import {EventCard} from '../EventCard'
 
-export const EventList = () => <></>
+export const EventList = forwardRef(({events}, ref) => (
+  <PublicationList publications={events} Card={EventCard} ref={ref} />
+))
+
+EventList.displayName = 'EventList'
+
+EventList.propTypes = {
+  events: EventConnectionList.isRequired,
+}
