61e503aa263824cc154df5e0ef7a4911493a8573
Toleckk <anatolich1995@gmail.com>
2020-10-10 00:15:47 +0300

    Add EventDisplay

--- src/features/common/organisms/EventsDisplay/EventsDisplay.js
+++ src/features/common/organisms/EventsDisplay/EventsDisplay.js
@@ -1,3 +1,17 @@
 import React from 'react'
+import {getEvents} from 'models/event'
+import {Loader} from 'ui'
+import {useMyQuery} from '../../hooks'
+import {EventList} from '../../molecules'
 
-export const EventsDisplay = () => <></>
+export const EventsDisplay = () => {
+  const {data, loading} = useMyQuery(getEvents({first: 25}))
+
+  if (loading) return <Loader />
+
+  const edges = data?.getEvents?.edges
+
+  if (!edges) return <></>
+
+  return <EventList events={edges} />
+}
