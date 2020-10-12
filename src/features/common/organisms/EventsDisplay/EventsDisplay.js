baa73f6120138afc1f6c09beb12a5ba192f4908c
Toleckk <anatolich1995@gmail.com>
2020-10-12 21:07:43 +0300

    Implement events infinite scrolling

--- src/features/common/organisms/EventsDisplay/EventsDisplay.js
+++ src/features/common/organisms/EventsDisplay/EventsDisplay.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import {getEvents} from 'models/event'
 import {Loader} from 'ui'
+import {useInfiniteScroll} from 'utils'
 import {useMyQuery} from '../../hooks'
 import {EventList} from '../../molecules'
 
 export const EventsDisplay = () => {
-  const {data, loading} = useMyQuery(getEvents({first: 25}))
-
-  if (loading) return <Loader />
-
+  const {data, loading, fetchMore} = useMyQuery(getEvents({first: 25}))
   const edges = data?.getEvents?.edges
 
-  if (!edges) return <></>
+  const ref = useInfiniteScroll({
+    fetchMore,
+    loading,
+    hasMore: data?.getEvents?.pageInfo?.hasNextPage,
+    threshold: '100px',
+  })
 
-  return <EventList events={edges} />
+  return loading ? <Loader /> : <EventList events={edges} ref={ref} />
 }
