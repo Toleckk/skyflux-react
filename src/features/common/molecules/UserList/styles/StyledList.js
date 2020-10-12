2da752044ab78a3805340aa8b3c26b7f451a27cd
Toleckk <anatolich1995@gmail.com>
2020-10-12 21:20:55 +0300

    Refactor UserList grid

--- src/features/common/molecules/UserList/styles/StyledList.js
+++ src/features/common/molecules/UserList/styles/StyledList.js
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 
 export const StyledList = styled.ul`
   display: grid;
-  grid-template-columns: min-content min-content min-content;
+  grid-template-columns: repeat(3, 30%);
   justify-content: space-between;
   grid-gap: 1rem;
 
   @media (min-width: 1366px) {
-    grid-template-columns: min-content min-content min-content min-content;
+    grid-template-columns: repeat(4, 20%);
   }
 `
