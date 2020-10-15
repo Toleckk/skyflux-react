9dda7a6ef892a69d716674f706b335da94e28733
Toleckk <anatolich1995@gmail.com>
2020-10-15 15:26:38 +0300

    Configure apollo client to use ws with subscriptions

--- src/configs/apollo.js
+++ src/configs/apollo.js
@@ -1,12 +1,46 @@
-import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client'
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client'
+import {getMainDefinition} from '@apollo/client/utilities'
+import {WebSocketLink} from '@apollo/client/link/ws'
 import {setContext} from '@apollo/client/link/context'
+
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_API_URL.replace(/^http/, 'ws') + '/graphql',
+  options: {
+    reconnect: true,
+    connectionParams: () => {
+      const token = localStorage.getItem('token')
+
+      return {
+        authorization: token ? `Bearer ${token}` : '',
+      }
+    },
+  },
+})
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_API_URL,
 })
 
+const splitLink = split(
+  ({query}) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink,
+)
+
 const authLink = setContext((_, {headers}) => {
   const token = localStorage.getItem('token')
+
   return {
     headers: {
       ...headers,
@@ -16,6 +50,6 @@ const authLink = setContext((_, {headers}) => {
 })
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: authLink.concat(splitLink),
   cache: new InMemoryCache(),
 })
