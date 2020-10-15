7d84811589d492296338a0635daf23b20da4d5df
Toleckk <anatolich1995@gmail.com>
2020-10-15 16:04:37 +0300

    Update comments connection on comment created

--- src/models/comment/queries.js
+++ src/models/comment/queries.js
@@ -7,23 +7,31 @@ import {
   GET_COMMENTS_BY_POST_ID,
 } from './schemas'
 
-export const getCommentsByPostId = (postId, variables = {}) => ({
-  query: GET_COMMENTS_BY_POST_ID,
-  variables: deepmerge({postId}, variables),
-})
+export const getCommentsByPostId = (postId, variables = {}) => {
+  const {subscription, variables: subVariables} = commentCreated(postId)
+
+  return {
+    query: GET_COMMENTS_BY_POST_ID,
+    variables: deepmerge({postId}, variables),
+    subscriptions: [
+      {
+        document: subscription,
+        variables: subVariables,
+        updateQuery: ({getCommentsByPostId}, {subscriptionData: {data}}) => ({
+          getCommentsByPostId: addNodeToConnection(
+            data.commentCreated,
+            getCommentsByPostId,
+          ),
+        }),
+      },
+    ],
+  }
+}
 
 export const createComment = (variables = {}) => ({
   mutation: CREATE_COMMENT,
   variables,
   refetchQueries: (_, {postId}) => [getPostById(postId)],
-  onCompleted: ({createComment: node}, {client: {cache}}) => {
-    cache.modify({
-      fields: {
-        [`getCommentsByPostId({"first":25,"post_id":"${node.post._id}"})`]: connection =>
-          addNodeToConnection(node, connection),
-      },
-    })
-  },
 })
 
 export const commentCreated = (postId, variables = {}) => ({
