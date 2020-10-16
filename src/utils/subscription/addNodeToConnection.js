9f80b2e2b2d31640944653ead8c2fb973feddd8e
Toleckk <anatolich1995@gmail.com>
2020-10-16 15:00:40 +0300

    Add node null check to addNodeToConnection

--- src/utils/subscription/addNodeToConnection.js
+++ src/utils/subscription/addNodeToConnection.js
@@ -1,7 +1,7 @@
 export const addNodeToConnection = (node, connection) => ({
   ...connection,
   edges: [
-    ...(connection.edges.some(({cursor}) => cursor === node._id)
+    ...(!node || connection.edges.some(({cursor}) => cursor === node._id)
       ? []
       : [{cursor: node._id, node}]),
     ...connection.edges,
