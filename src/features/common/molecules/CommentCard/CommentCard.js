1089a0e5dad1048f59c2e8558a831d507b6e88ec
Toleckk <anatolich1995@gmail.com>
2020-10-17 12:08:17 +0300

    Implement comment deleting

--- src/features/common/molecules/CommentCard/CommentCard.js
+++ src/features/common/molecules/CommentCard/CommentCard.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import {withTranslation} from 'react-i18next'
-import {Comment} from 'models/comment'
+import {Comment, deleteComment} from 'models/comment'
 import {Link, Tip} from 'ui'
+import {useIsMe, useMyMutation} from '../../hooks'
 import {PublicationCard} from '..'
 
-export const CommentCard = withTranslation('post')(({publication, t}) => (
-  <PublicationCard publication={publication}>
-    {!!publication.post && (
-      <div>
-        <Tip>
-          {t('To post')}&nbsp;
-          <Link to={`/post/${publication.post._id}`}>
-            {publication.post.text}
-          </Link>
-        </Tip>
-      </div>
-    )}
-  </PublicationCard>
-))
+export const CommentCard = withTranslation('post')(({publication, t}) => {
+  const isMyComment = useIsMe(publication.user)
+  const isMyPost = useIsMe(publication.post.user)
+
+  const [remove] = useMyMutation(deleteComment({_id: publication._id}))
+
+  const onDelete = useMemo(
+    () => (isMyComment || isMyPost) && (() => remove()),
+    [isMyComment, isMyPost, remove],
+  )
+
+  return (
+    <PublicationCard publication={publication} onDelete={onDelete}>
+      {!!publication.post && !!publication.post.text && (
+        <div>
+          <Tip>
+            {t('To post')}&nbsp;
+            <Link to={`/post/${publication.post._id}`}>
+              {publication.post.text}
+            </Link>
+          </Tip>
+        </div>
+      )}
+    </PublicationCard>
+  )
+})
 
 CommentCard.propTypes = {
   publication: Comment.isRequired,
