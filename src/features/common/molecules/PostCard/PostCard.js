b06c33cd72d2a2f5377672cd246a46f705c00690
Toleckk <anatolich1995@gmail.com>
2020-10-18 20:03:11 +0300

    Implement post deleting confirmation

--- src/features/common/molecules/PostCard/PostCard.js
+++ src/features/common/molecules/PostCard/PostCard.js
@@ -1,17 +1,22 @@
 import React, {useCallback} from 'react'
 import {Box, Flex} from 'reflexbox/styled-components'
 import {Link} from 'react-router-dom'
+import {useTranslation} from 'react-i18next'
 import {Icon, Text} from 'ui'
 import {deletePost, Post} from 'models/post'
 import {createLike, deleteLike} from 'models/like'
-import {useIsMe, useMyMutation} from '../../hooks'
+import {useConfirmDialog, useIsMe, useMyMutation} from '../../hooks'
 import {PublicationCard} from '../PublicationCard'
 
 export const PostCard = ({publication}) => {
+  const {t} = useTranslation('post')
+
   const isMe = useIsMe(publication.user)
   const [like] = useMyMutation(createLike({postId: publication._id}))
   const [unlike] = useMyMutation(deleteLike({postId: publication._id}))
   const [remove] = useMyMutation(deletePost({_id: publication._id}))
+
+  const [removeWithConfirmation, Modal] = useConfirmDialog(remove)
 
   const onClick = useCallback(
     () => (publication.isLikedByMe ? unlike() : like()),
@@ -21,7 +26,7 @@ export const PostCard = ({publication}) => {
   return (
     <PublicationCard
       publication={publication}
-      onDelete={isMe && (() => remove())}
+      onDelete={isMe && (() => removeWithConfirmation())}
     >
       <Flex justifyContent="space-between">
         <Flex alignItems="center" as={Link} to={'/post/' + publication._id}>
@@ -37,6 +42,10 @@ export const PostCard = ({publication}) => {
           </Box>
         </Flex>
       </Flex>
+      <Modal
+        text={t('Are you sure you want to delete this post?')}
+        icon="trash"
+      />
     </PublicationCard>
   )
 }
