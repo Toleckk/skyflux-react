9fdeda9640cbc4c4ca4f3ecd313daaa26f7de9a9
Toleckk <anatolich1995@gmail.com>
2020-10-18 19:35:35 +0300

    Add onClose modal prop

--- src/ui/Modal/Modal.js
+++ src/ui/Modal/Modal.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import noop from 'noop6'
 import {useMedia} from 'react-use'
 import {StyledDrawer} from './styles'
 
-export const Modal = ({placement, children, visible}) => {
+export const Modal = ({placement, children, visible, onClose}) => {
   const isDesktop = useMedia('(min-width: 768px)')
   const props = isDesktop ? {width: 'auto', height: 'auto'} : {}
 
   return (
-    <StyledDrawer placement={placement} visible={visible} {...props}>
+    <StyledDrawer
+      placement={placement}
+      visible={visible}
+      onClose={onClose}
+      {...props}
+    >
       {children}
     </StyledDrawer>
   )
@@ -16,12 +22,14 @@ export const Modal = ({placement, children, visible}) => {
 
 Modal.defaultProps = {
   placement: 'bottom',
+  onClose: noop,
   visible: false,
   children: null,
 }
 
 Modal.propTypes = {
   placement: PropTypes.oneOf(['bottom', 'top', 'left', 'right']),
+  onClose: PropTypes.func,
   visible: PropTypes.bool,
   children: PropTypes.node,
 }
