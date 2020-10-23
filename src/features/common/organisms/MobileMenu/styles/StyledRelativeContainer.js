a492660189679cb9f039ca3b7231ac4fe29401cc
Toleckk <anatolich1995@gmail.com>
2020-10-23 18:21:24 +0300

    Inject SwitchThemeButton into MobileMenu

---
+++ src/features/common/organisms/MobileMenu/styles/StyledRelativeContainer.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledRelativeContainer = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-top: 2rem;
+`
