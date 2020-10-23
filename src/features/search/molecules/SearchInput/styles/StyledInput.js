e44267f0ee3988f591fd7d333a8714fc219e3613
Toleckk <anatolich1995@gmail.com>
2020-10-23 10:29:21 +0300

    Remove invalid search input background

--- src/features/search/molecules/SearchInput/styles/StyledInput.js
+++ src/features/search/molecules/SearchInput/styles/StyledInput.js
@@ -11,6 +11,7 @@ export const StyledInput = styled.input`
   padding: 0 4rem 0 1rem;
 
   color: rgb(${props => props.theme.primaryText});
+  background: none;
 
   border: 2px solid rgba(${props => props.theme.primaryText});
   border-radius: 2rem;
