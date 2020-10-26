9af6fe54659f450b82ba9d896ad30e8ab529e980
Toleckk <anatolich1995@gmail.com>
2020-10-27 00:09:24 +0300

    Fix user page header adaptivity

--- src/features/user/pages/Wall/styles/StyledHeader.js
+++ src/features/user/pages/Wall/styles/StyledHeader.js
@@ -3,7 +3,12 @@ import styled from 'styled-components'
 export const StyledHeader = styled.header`
   position: fixed;
   top: 0;
-  width: 55vw;
 
   background: rgb(${props => props.theme.primaryDark});
+
+  width: 100vw;
+
+  @media (min-width: 768px) {
+    width: 55vw;
+  }
 `
