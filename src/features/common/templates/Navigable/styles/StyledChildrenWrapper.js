9e86db6d879f641f7880243d9df74c099a7d3dc3
Toleckk <anatolich1995@gmail.com>
2020-10-27 00:17:14 +0300

    Fix Navigable template adaptivity

---
+++ src/features/common/templates/Navigable/styles/StyledChildrenWrapper.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import {MOBILE_NAV_HEIGHT} from '../../../organisms'
+
+export const StyledChildrenWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  min-height: 100vh;
+
+  width: calc(100vw - 1rem);
+  padding-bottom: ${MOBILE_NAV_HEIGHT};
+
+  @media (min-width: 768px) {
+    width: 55vw;
+    padding-bottom: 1rem;
+  }
+`
