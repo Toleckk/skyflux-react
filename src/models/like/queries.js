84e0c83ffc735096269bda24e7a12174e2953fe1
Toleckk <anatolich1995@gmail.com>
2020-11-05 12:43:45 +0300

    Add like's subscriptions

--- src/models/like/queries.js
+++ src/models/like/queries.js
@@ -1,5 +1,6 @@
+import deepmerge from 'deepmerge'
 import {getPostById} from 'models/post'
-import {CREATE_LIKE, DELETE_LIKE} from './schemas'
+import {CREATE_LIKE, DELETE_LIKE, LIKE_CREATED, LIKE_DELETED} from './schemas'
 
 export const createLike = (variables = {}) => ({
   mutation: CREATE_LIKE,
@@ -11,4 +12,14 @@ export const deleteLike = (variables = {}) => ({
   mutation: DELETE_LIKE,
   variables,
   refetchQueries: (_, variables) => [getPostById(variables.postId)],
+})
+
+export const likeCreated = (postId, variables = {}) => ({
+  subscription: LIKE_CREATED,
+  variables: deepmerge({postId}, variables),
+})
+
+export const likeDeleted = (postId, variables = {}) => ({
+  subscription: LIKE_DELETED,
+  variables: deepmerge({postId}, variables),
 })
--- src/models/like/schemas.js
+++ src/models/like/schemas.js
@@ -1,5 +1,16 @@
 import {gql} from '@apollo/client'
 
+export const LikeFragment = gql`
+  fragment LikeFragment on Like {
+    _id
+    user {
+      _id
+      avatar
+      nickname
+    }
+  }
+`
+
 export const CREATE_LIKE = gql`
   mutation createLike($postId: ID!) {
     createLike(post_id: $postId) {
@@ -11,5 +22,31 @@ export const CREATE_LIKE = gql`
 export const DELETE_LIKE = gql`
   mutation deleteLike($postId: ID!) {
     deleteLike(post_id: $postId)
+  }
+`
+
+export const LIKE_CREATED = gql`
+  subscription likeCreated($postId: ID!) {
+    likeCreated(post_id: $postId) {
+      _id
+      post {
+        _id
+        isLikedByMe
+        likesCount
+      }
+    }
+  }
+`
+
+export const LIKE_DELETED = gql`
+  subscription likeDeleted($postId: ID!) {
+    likeDeleted(post_id: $postId) {
+      _id
+      post {
+        _id
+        isLikedByMe
+        likesCount
+      }
+    }
   }
 `
