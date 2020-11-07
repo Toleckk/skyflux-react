2504c1c87c80436ea213390cd9184c4831ca400e
Toleckk <anatolich1995@gmail.com>
2020-11-07 12:31:37 +0300

    Do not render PostDisplay if _id is not set

--- src/features/post/pages/PostModal/PostModal.js
+++ src/features/post/pages/PostModal/PostModal.js
@@ -6,15 +6,13 @@ import {PostDisplay} from '../../organisms'
 import {StyledContainer} from './styles'
 
 export const PostModal = ({_id, visible, onClose}) => (
-  <Modal onClose={onClose} visible={visible}>
-    <StyledContainer>
-      <PostDisplay _id={_id} />
-    </StyledContainer>
+  <Modal onClose={onClose} visible={visible && _id}>
+    <StyledContainer>{_id && <PostDisplay _id={_id} />}</StyledContainer>
   </Modal>
 )
 
 PostModal.propTypes = {
-  _id: ID.isRequired,
+  _id: ID,
   visible: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
 }
