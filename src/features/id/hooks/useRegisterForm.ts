e3a6ee1a29de778b2cca345bf45dd9dbda591bf8
Toleckk <anatolich1995@gmail.com>
2020-12-01 13:05:21 +0300

    Redirect to login after registration

--- src/features/id/hooks/useRegisterForm.ts
+++ src/features/id/hooks/useRegisterForm.ts
@@ -3,6 +3,7 @@ import {useMutation} from '@apollo/client'
 import {useForm} from 'react-hook-form'
 import {yupResolver} from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import {useHistory} from 'react-router'
 import {email, password} from '@skyflux/react/validation'
 import {CustomFormHookResult, mergeErrors} from '@skyflux/react/utils'
 import {CREATE_USER, CreateUserVariables} from '../graphql'
@@ -15,7 +16,10 @@ const schema = yup.object().shape({
 })
 
 export const useRegisterForm = (): UseRegisterFormResult => {
-  const [signUp, {error, loading}] = useMutation(CREATE_USER)
+  const history = useHistory()
+  const [signUp, {error, loading}] = useMutation(CREATE_USER, {
+    onCompleted: data => data?.createUser?._id && history.push('/id/auth'),
+  })
 
   const {handleSubmit, errors, ...rest} = useForm<CreateUserVariables>({
     resolver: yupResolver(schema),
