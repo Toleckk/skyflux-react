311bd609bca8158f1f58f94e1bf91ec9a0e704b9
Toleckk <anatolich1995@gmail.com>
2020-12-14 20:14:19 +0300

    Add introspection url env param

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,10 @@ module.exports = {
   client: {
     service: {
       name: 'api',
-      url: process.env.REACT_APP_API_URL,
+      url:
+        process.env.INTROSPECTION_URL ||
+        process.env.REACT_APP_API_URL ||
+        'http://localhost:4000',
     },
   },
 }
